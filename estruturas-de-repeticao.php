b37f27fc9c4aae851997a96e8e2361cc57f7ffa2
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-06-04 16:14:11 -0300

    Create estruturas-de-repeticao.php

---
+++ estruturas-de-repeticao.php
@@ -0,0 +1,11 @@
+<?php
+for
+for ($cont = 0; $cont < 10; $cont++) {
+     echo "$cont \n";
+}
+
+for com array
+ $arrayFrutas = Array("Laranja", "Maça", "Limão");
+ for ($i=0; $i<count($arrayFrutas); $i++) {
+     echo "Fruta: " . $arrayFrutas[$i] . "\n";
+}
